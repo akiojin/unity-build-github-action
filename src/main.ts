24cb42449e9b1bc0302b1a0858dec3ca31c944ea
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-03-14 14:24:28 +0900

    初期コミット

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as os from 'os'
+
+function GetUnityVersionPath(os: string, unityVersion: string)
+{
+	switch (os) {
+	default:
+	case 'darwin':
+		return `/Applications/Unity/Hub/Editor/${unityVersion}/Unity.app/Contents/MacOS/Unity`
+	case 'win32':
+		return `C:\\Program Files\\Unity\\Hub\\Editor\\${unityVersion}\\Editor\\Unity.exe`
+	}
+}
+
+async function Run()
+{
+	try {
+		const unityVersion = core.getInput('unity-version')
+		const buildTarget = core.getInput('build-target')
+		const projectDirectory = core.getInput('project-directory')
+		const outputDirectory = core.getInput('output-directory')
+
+		var args = [
+			'-quit',
+			'-batchmode',
+			'-nographics',
+			'-silent-crashes',
+			'-buildTarget', buildTarget,
+			'-projectPath', projectDirectory,
+			'-outputPath', outputDirectory,
+			'-logFile', core.getInput('log-file')
+		]
+
+		if (!!core.getBooleanInput('disable-upm')) {
+			args.push('-noUpm')
+		}
+
+		if (core.getInput('execute-method') !== '') {
+			args.push('-executeMethod')
+			args.push(core.getInput('execute-method'))
+		}
+
+		if (core.getInput('additional-arguments') !== '') {
+			args.push(core.getInput('additional-arguments'))
+		}
+
+		await exec.exec(GetUnityVersionPath(os.platform(), unityVersion), args)
+	} catch (ex: any) {
+		core.setFailed(ex.message)
+	}
+}
+
+Run()
