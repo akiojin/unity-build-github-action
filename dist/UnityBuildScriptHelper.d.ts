8c67b0926421c0a8f2fd3f838211a6e767361bb5
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2023-03-17 14:45:32 +0900

    fix: tmpをTypeScriptで使う場合は@types/tmpも必要

--- dist/UnityBuildScriptHelper.d.ts
+++
@@ -1,4 +0,0 @@
-export default class UnityBuildScriptHelper {
-    static ToTitleCase(str?: string): string;
-    static GenerateUnityBuildScript(outputDirectory: string, outputFileName: string, buildTarget: string, revision: number, development?: boolean, teamID?: string, provisioningProfileUUID?: string, provisioningProfileType?: string, enableBitcode?: boolean, keystore?: string, keystorePassword?: string, keystoreAlias?: string, keystoreAliasPassword?: string): string;
-}
