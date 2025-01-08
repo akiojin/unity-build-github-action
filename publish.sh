ffbcd5928f3ca593db80e5bffda6b3d4b9910731
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2025-01-08 10:29:03 +0900

    fix: update

--- publish.sh
+++ publish.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ] || [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" && "$1" != "prerelease" ]]; then
+if [ "$#" -ne 1 ] || [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" ]]; then
     echo "Usage:"
-    echo " ./publish.sh <major/minor/patch/prerelease>"
+    echo " ./publish.sh <major/minor/patch>"
     echo "  or"
     echo " npm run release <major/minor/patch>"
-    echo "  or"
-    echo " npm run pre-release"
     exit 1
 fi
 
@@ -25,43 +23,31 @@ if ! npm run build; then
     exit 1
 fi
 
-STATUS=$(git status --porcelain)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [ -n "$STATUS" ]; then
-    echo "Error: There are uncommitted files. Please commit them first."
+if [ "$BRANCH" != "develop" ]; then
+    echo "Error: You must be on the develop branch to publish."
     exit 1
 fi
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [ "$1" != "prerelease" ]; then
-    if [ "$BRANCH" != "develop" ]; then
-        echo "Error: You must be on the develop branch to publish."
-        exit 1
-    fi
-
-    VERSION=$(npm version $1 --no-git-tag-version)
-else
-    if [ "$BRANCH" == "main" ] || [ "$BRANCH" == "develop" ]; then
-        echo "Error: prerlease must be outside the main or develop branch."
-        exit 1
-    fi
-
-    VERSION=$(npm version prerelease --preid rc)
-fi
+VERSION=$(npm version $1 --no-git-tag-version)
 
 if ! git pull; then
     echo "Error: Failed to pull the latest changes."
     exit 1
 fi
 
+git branch release/$VERSION
+git switch release/$VERSION
 git add package.json package-lock.json
 git commit -m "bump: $VERSION"
-git push --follow-tags
+git push --follow-tags --set-upstream origin release/$VERSION
 
-if [ "$1" != "prerelease" ]; then
-    if ! gh pr create --base main --head develop --title "bump: $VERSION" --body "bump: $VERSION"; then
-        echo "Error: Failed to create a pull request."
-        exit 1
-    fi
+if ! gh pr create --base main --head release/$VERSION --title "bump: $VERSION" --body "bump: $VERSION"; then
+    echo "Error: Failed to create a pull request."
+    exit 1
 fi
+
+git switch develop
+git merge release/$VERSION --no-ff --no-edit
+git push --follow-tags
